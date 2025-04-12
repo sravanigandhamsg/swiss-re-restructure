4e19e2df056d5453566f281cea5cf3e3a11df6f0
sravanigandhamsg <sravanigandhamsg@gmail.com>
2025-04-12 22:45:29 +0530

    Added Employee model

---
+++ src/main/java/org/example/model/Employee.java
@@ -0,0 +1,53 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double salary;
+    private Integer managerId;
+    private List<Employee> subordinates = new ArrayList<>();
+
+    public Employee(int Id, String FirstName, String LastName, double Salary, int ManagerId){
+        this.id = Id;
+        this.firstName = FirstName;
+        this.lastName = LastName;
+        this.salary = Salary;
+        this.managerId = ManagerId;
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public void setSubordinates(List<Employee> subordinates) {
+        this.subordinates = subordinates;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+}
